9bfac048d63a00ad4f2c1d3df244b06329596a71
Minjae <pandalee1147@gmail.com>
2023-08-11 09:28:14 +0900

    security change

--- prototype/src/main/java/com/ringcook/prototype/SecurityConfig.java
+++ prototype/src/main/java/com/ringcook/prototype/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.ringcook.prototype;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
